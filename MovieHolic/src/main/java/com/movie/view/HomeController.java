d8e6d1c68656001e842bee14a88111e0f7356b9e
JangWookHeo <jwheo1030@gmail.com>
2023-06-02 08:54:54 +0900

    프로젝트_준비

---
+++ MovieHolic/src/main/java/com/movie/view/HomeController.java
@@ -0,0 +1,26 @@
+package com.movie.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String home(Model model) {
+		// 영화 목록 조회 (예시)
+		// List<MoiveVO> bestMovieList = movieService.getBestMoveiList();
+		// model.addAttribute("bestMovieList", bestMovieList);
+		
+		return "index";
+	}
+	
+}
